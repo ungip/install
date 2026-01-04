b4633947ba1edd664a2858e05d495fe5edc47937
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-04 17:39:05 +0100

    Update yay.sh

--- arch/yay.sh
+++ arch/yay.sh
@@ -1,2 +1,1 @@
-!#/bin/bash
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+cd ~/Downloads && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
