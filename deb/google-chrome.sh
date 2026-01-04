77c3d1efe56a51e443c35e2c3d5b93405a277ea3
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-04 17:37:51 +0100

    Update google-chrome.sh

--- deb/google-chrome.sh
+++ deb/google-chrome.sh
@@ -1,1 +1,1 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+cd ~/Downloads && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i ./google-chrome-stable_current_amd64.deb
