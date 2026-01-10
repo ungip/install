c9dcd571763cf7469a913f901718eb942dc18643
ungip <ungvolgyi.peter@gmail.com>
2026-01-10 18:20:46 +0100

    update

--- arch/yay.sh
+++ arch/yay.sh
@@ -1,1 +1,3 @@
-cd ~/Downloads && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+#!/bin/sh
+
+cd && mkdir temp && cd temp && sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay-bin && cd yay-bin && makepkg -si
--- deb/google-chrome.sh
+++ deb/google-chrome.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 cd ~/Downloads && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i ./google-chrome-stable_current_amd64.deb
