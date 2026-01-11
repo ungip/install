4cea2b0dc062da9bdfec7d82a5e851fa1628c3ca
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-11 11:30:38 +0100

    Update misc.sh

--- hyprland/misc.sh
+++ hyprland/misc.sh
@@ -5,6 +5,8 @@ misc=(
     micro
     exa
     otf-font-awesome
+    7zip
+    xarchiver
 )
 
 sudo pacman -S --noconfirm --needed "${misc[@]}" && printf "set -U fish_greeting" | sudo tee ~/.config/fish/config.fish
