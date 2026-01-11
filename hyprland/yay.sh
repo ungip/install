fe24923efe68f897e96bfbf6a5fe338765100cf3
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-11 09:32:12 +0100

    Update yay.sh

--- hyprland/yay.sh
+++ hyprland/yay.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd && mkdir temp && cd temp && sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay-bin && cd yay-bin && makepkg -si
+cd && sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay-bin && cd yay-bin && makepkg -si
