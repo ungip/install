c8d39eb06817899c5aa3e72ef279f5decf0660b2
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-11 09:31:08 +0100

    Update sddm.sh

--- hyprland/sddm.sh
+++ hyprland/sddm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S --noconfirm --needed sddm tee && sudo systemct enable sddm && printf "[General]\nNumlock=on" | sudo tee /etc/sddm.conf
+sudo pacman -S --noconfirm --needed sddm && sudo systemct enable sddm
