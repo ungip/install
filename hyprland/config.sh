873dee21cce8edd0db648a7141f3fe3a41a8bc17
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-11 19:26:18 +0100

    Update config.sh

--- hyprland/config.sh
+++ hyprland/config.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 
 sudo sh -c 'printf "set -U fish_greeting" >> ~/.config/fish/config.fish'
-sudo sh -c 'printf "[General]\nNumlock=on" >> /etc/sddm.conf'
+sudo sh -c 'printf "[General]\nNumlock=on\n\n[Autologin]\nUser=ungi\nSession=hyprland" >> /etc/sddm.conf'
