66b1b600ba436accf567693de046d62aae30e2ad
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-13 19:34:56 +0100

    Update config.sh

--- hyprland/config.sh
+++ hyprland/config.sh
@@ -4,5 +4,7 @@ mkdir -p ~/.config/hypr && cp -Rf ./config/hypr/* ~/.config/hypr/
 mkdir -p ~/.config/rofi && cp -Rf ./config/rofi/* ~/.config/rofi/
 mkdir -p ~/.config/waybar && cp -Rf ./config/waybar/* ~/.config/waybar/
 mkdir -p ~/.config/kitty && cp -Rf ./config/kitty.conf ~/.config/kitty/
+mkdir -p ~/.local/share/fonts && cp -Rf ./config/MesloLGLNerdFont-Regular.ttf ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts && cp -Rf ./config/MesloLGLNerdFontMono-Regular.ttf ~/.local/share/fonts
 sudo sh -c 'printf "set -U fish_greeting" >> ~/.config/fish/config.fish'
 sudo sh -c 'printf "[General]\nNumlock=on\n\n[Autologin]\nUser=ungi\nSession=hyprland" >> /etc/sddm.conf'
