8363a78374b1a9345d48ccc3bb92804e51a3e424
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-17 16:49:15 +0100

    updt

--- hyprland/config.sh
+++ hyprland/config.sh
@@ -7,6 +7,7 @@ mkdir -p ~/.config/kitty && cp -Rf ./config/kitty.conf ~/.config/kitty/
 mkdir -p ~/.local/share/fonts && cp -Rf ./config/MesloLGLNerdFont-Regular.ttf ~/.local/share/fonts
 mkdir -p ~/.local/share/fonts && cp -Rf ./config/MesloLGLNerdFontMono-Regular.ttf ~/.local/share/fonts
 mkdir -p ~/wallpaper && cp -Rf ./wallpaper/* ~/wallpaper
+mkdir -p ~/.local/share/icons && 7z x ./config/capitaine-light.7z -o$HOME/.local/share/icons/
 sudo sh -c 'printf "[General]\nNumlock=on\n\n[Autologin]\nUser=ungi\nSession=hyprland" > /etc/sddm.conf'
 cd && sudo sh -c 'printf "set -U fish_greeting" >> .config/fish/config.fish'
 
