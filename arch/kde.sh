02bfe99b826a93bd2d962c68b88e4cf3c1aa45ca
Ungi <102828511+ungip@users.noreply.github.com>
2026-01-20 18:46:02 +0100

    upd

--- arch/kde.sh
+++ arch/kde.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S -- needed okular kwalletmanager kio-admin print-manager system-config-printer
+sudo pacman -S --needed okular kwalletmanager gwenview kio-admin print-manager system-config-printer
