2878e0e6f147de715f5e35db9f990999b53fb9a3
Ungi <102828511+ungip@users.noreply.github.com>
2026-04-08 16:20:49 +0200

    Update KDE package installation in kde.sh

--- arch/kde.sh
+++ arch/kde.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S --needed okular kwalletmanager gwenview kio-admin print-manager system-config-printer
+sudo pacman -S --needed okular plasma-pa kio-admin kwalletmanager kscreen bluedevil print-manager system-config-printer kinfocenter breeze-gtk kde-gtk-config kdeplasma-addons discover
