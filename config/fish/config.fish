c4751eb949ad7fa9aaaf91b932b8715aa47cfb30
Ungi <102828511+ungip@users.noreply.github.com>
2026-04-09 09:01:09 +0200

    update

---
+++ config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+set -U fish_greeting
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
