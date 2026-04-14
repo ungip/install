02e3228a46ba1914c754e7b9afdff06dc431ff73
Ungi <102828511+ungip@users.noreply.github.com>
2026-04-14 08:04:13 +0200

    Add kcalc to KDE installation script

--- arch/kde.sh
+++ arch/kde.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S --needed okular plasma-pa kio-admin kwalletmanager kscreen bluedevil print-manager system-config-printer kinfocenter breeze-gtk kde-gtk-config kdeplasma-addons discover
+sudo pacman -S --needed okular plasma-pa kio-admin kwalletmanager kscreen bluedevil print-manager system-config-printer kinfocenter breeze-gtk kde-gtk-config kdeplasma-addons discover kcalc
